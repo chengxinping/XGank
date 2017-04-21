f139c8724fd1a1acf98d15b907402b39bd6aad38
XinPingCheng <972649353@qq.com>
2017-04-21 14:36:32 +0800

    项目分包

--- app/src/main/java/cn/xpcheng/xgank/ui/activity/MainActivity.java
+++ app/src/main/java/cn/xpcheng/xgank/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package cn.xpcheng.xgank;
+package cn.xpcheng.xgank.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import cn.xpcheng.xgank.R;
 
 public class MainActivity extends AppCompatActivity {
 
